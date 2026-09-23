50dee4d29fc666647c96aec12afcad6768369783
a-grow <growandygrow@gmail.com>
2026-09-23 16:09:56 +0800

    Grammar Dig: Level 2 (Plurals) pool + level plumbing

--- src/pages/GrammarDig.tsx
+++ src/pages/GrammarDig.tsx
@@ -34,7 +34,7 @@ export default function GrammarDig() {
       <iframe
         ref={frameRef}
         onLoad={() => frameRef.current?.contentWindow?.focus()}
-        src="/Teacher_Andy_Dig_game.html"
+        src={`/Teacher_Andy_Dig_game.html?level=${level || 1}`}
         title="Teacher Andy Dig"
         style={{ width: "100%", height: "100%", border: "none", display: "block" }}
       />
